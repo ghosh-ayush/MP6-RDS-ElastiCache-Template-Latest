9698f013c269213321de4ea7aeeb2c698859875a
Bala Sukesh <bbelide2@illinois.edu>
2023-04-01 14:56:06 -0500

    Update mp6_template.py

--- mp6_template.py
+++ mp6_template.py
@@ -5,14 +5,14 @@ import redis
 import pymysql
 
 
-DB_HOST = "database-3.cluster-ro-ck7lt9uxmu6g.us-east-1.rds.amazonaws.com"
-DB_USER = "admin"
-DB_PASS = "abc12345"
-DB_NAME = "mpdb"
-DB_TABLE = "herostb"
-REDIS_URL = "redis://elc-tutorial.tlzedr.ng.0001.use1.cache.amazonaws.com:6379"
+DB_HOST = " " # Add DB end point
+DB_USER = " " # Add your database user
+DB_PASS = " " # Add your database password
+DB_NAME = " " # Add your database name
+DB_TABLE = " " # Add your table name
+REDIS_URL = " " # Add redis end point "redis://<end point>"
 
-TTL = 10
+TTL = 60
 
 class DB:
     def __init__(self, **params):
@@ -26,13 +26,18 @@ class DB:
             cursor.execute(sql)
             return cursor.fetchall()
 
+    def record(self, sql, values):
+        with self.mysql.cursor() as cursor:
+            cursor.execute(sql, values)
+            return cursor.fetchone()
+
     def get_idx(self, table_name):
         with self.mysql.cursor() as cursor:
             cursor.execute(f"SELECT MAX(id) as id FROM {table_name}")
             idx = str(cursor.fetchone()['id'] + 1)
             return idx
 
-    def insert(self, idx, data):
+    def insert(self, idx, data, table_name):
         with self.mysql.cursor() as cursor:
             hero = data["hero"]
             power = data["power"]
@@ -40,33 +45,36 @@ class DB:
             xp = data["xp"]
             color = data["color"]
             
-            sql = f"INSERT INTO herostb (`id`, `hero`, `power`, `name`, `xp`, `color`) VALUES ('{idx}', '{hero}', '{power}', '{name}', '{xp}', '{color}')"
-
+            sql = f"INSERT INTO {table_name} (`id`, `hero`, `power`, `name`, `xp`, `color`) VALUES ('{idx}', '{hero}', '{power}', '{name}', '{xp}', '{color}')"
             cursor.execute(sql)
             self.mysql.commit()
 
-def read(use_cache, indices, Database, Cache):
+def read(use_cache, xps, Database, Cache):
+    # Implement Lazy Loading strategy
     
-    if use_cache:
-
-    else:
-
+    result = []
     
+    if use_cache: # Use cache
+      
+       
+    else: # Do not use cache
+    
+    return result
     
 def write(use_cache, sqls, Database, Cache):
+    # Implement Write Through strategy
     
-    if use_cache:
-        # write through strategy
-
-    else:
-
+    if use_cache: # Use cache
+       
+    else: # Do not use cache
+        
 
 def lambda_handler(event, context):
     
     USE_CACHE = (event['USE_CACHE'] == "True")
     REQUEST = event['REQUEST']
     
-    # initialize database and cache
+    # Initialize database
     try:
         Database = DB(host=DB_HOST, user=DB_USER, password=DB_PASS, db=DB_NAME)
     except pymysql.MySQLError as e:
@@ -74,14 +82,16 @@ def lambda_handler(event, context):
         print(e)
         sys.exit()
         
+    # Initialize cache
     Cache = redis.Redis.from_url(REDIS_URL)
     
     result = []
     if REQUEST == "read":
-        # event["SQLS"] should be a list of integers
+        # event["SQLS"] is a list of all xps for which you have to query the database or redis.
         result = read(USE_CACHE, event["SQLS"], Database, Cache)
+        
     elif REQUEST == "write":
-        # event["SQLS"] should be a list of jsons
+        # event["SQLS"] should be a list of jsons. You have to write these rows to the database.
         write(USE_CACHE, event["SQLS"], Database, Cache)
         result = "write success"
     
